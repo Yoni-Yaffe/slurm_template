2ea8841302dfd8fcc6094caa6982195de6e35e1f
Yoni-Yaffe <jonathany@mail.tau.ac.il>
2025-05-20 17:46:42 +0300

    added submit messge in send_to_slurm.py

--- send_to_slurm.py
+++ send_to_slurm.py
@@ -12,11 +12,7 @@ DEFAULT_LOG_DIR = '/vol/scratch/USER/runs'
 def submit_task(config: dict, args:argparse.Namespace):
     config = copy.deepcopy(config)
     slurm_config = config['slurm_params']
-    
-    if 'logdir' in config:
-        logdir_base = config['logdir']
-    else:
-        logdir_base = DEFAULT_LOG_DIR
+    logdir_base = config.get('logdir', DEFAULT_LOG_DIR) 
     run_name = f"{datetime.now().strftime('%y%m%d-%H%M%S')}_{config['run_name']}"
     logdir = os.path.join(logdir_base, run_name) # ckpts and midi will be saved here
     cmd = ['sbatch', f'--export=ALL,LOGDIR={logdir}']
@@ -34,20 +30,17 @@ def submit_task(config: dict, args:argparse.Namespace):
         cmd.extend([f'--{opt}', str(val)])
         
     cmd.extend([config['command'], '--logdir', logdir, '--yaml_config', new_yaml_path])
-    local_command = f"python3 main.py --logdir {logdir} --yaml_config {new_yaml_path}"
-    if 'local' in config and config['local']:
-        os.system(local_command)
-    else:
-        if args.dry_run:
-            print(' '.join(cmd))
-            return
-        result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        job_id = result.stdout.strip().split()[-1]
-        with open(os.path.join(logdir, 'job_launch.log'), 'a') as f:
-            f.write("Job ID: " + job_id + '\n')
-        print("Submitted SLURM job:", job_id)
-
-
+    if args.dry_run:
+        print(' '.join(cmd))
+        return
+    result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    job_id = result.stdout.strip().split()[-1]
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open(os.path.join(logdir, 'job_launch.log'), 'a') as f:
+        f.write(f"[SUBMITTED] JobID {job_id} at {timestamp}\n-----------------------\n")
+    print("Submitted SLURM job:", job_id)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Submit a SLURM job or run a task locally based on a YAML config.")
